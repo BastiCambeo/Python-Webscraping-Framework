a85c9e7ca75e6b9b26d24cfb0811dfb43ba2a21f
BastiCambeo <Sebastian@cambeo.com>
2014-03-10 21:15:46 +0100

    documentation added

--- applications/welcome/models/Controller.py
+++ applications/welcome/models/Controller.py
@@ -1,10 +1,13 @@
 def parse(html_code, xpath, regex=".*"):
-    from lxml import html
-    import re
+    """ Parses an html document for a given XPath expression. Any resulting node can optionally be filtered against a regular expression """
+    from lxml import html  # xpath support
+    import re  # regex support
     parsed_tree = html.document_fromstring(html_code)
     return [re.search(regex, node,  re.DOTALL).group() for node in parsed_tree.xpath(xpath)]
 
+
 def load(url):
-    import urllib2
+    """ Returns the response of an http get-request to a given url """
+    import urllib2  # http support
     response = urllib2.urlopen(url)
     return response.read()
