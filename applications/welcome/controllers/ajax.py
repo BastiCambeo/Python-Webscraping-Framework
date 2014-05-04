78b41af1e9a58e00010080a38c5046b6312be30e
BastiCambeo <Sebastian@cambeo.com>
2014-05-04 18:02:19 +0200

    Support for multiple rows, more regex intelligence, auto date recognition
    Leichtathletik example extended to name, time, date (format is
    automatically deduced)

--- applications/welcome/controllers/ajax.py
+++ applications/welcome/controllers/ajax.py
@@ -1,11 +1,15 @@
-def add_task():
-    task = Storage(
+task = Storage(
         name="Leichtatlethik",
         url="http://www.iaaf.org/records/toplists/sprints/100-metres/outdoor/men/senior/2013",
-        xpath="""//table[@class = "records-table toggled-table condensedTbl"]/tr[@id]/td[2]/text()""",
-        regex="\d+\.\d+",
+        ## first name, last name, time, date ##
+        selectors = [Selector(xpath="""//table[@class = "records-table toggled-table condensedTbl"]/tr[@id]/td[4]/a/text()""", type=unicode),
+                     Selector(xpath="""//table[@class = "records-table toggled-table condensedTbl"]/tr[@id]/td[4]/a/span/text()""", type=unicode),
+                     Selector(xpath="""//table[@class = "records-table toggled-table condensedTbl"]/tr[@id]/td[2]/text()""", type=float),
+                     Selector(xpath="""//table[@class = "records-table toggled-table condensedTbl"]/tr[@id]/td[9]/text()""", type=datetime.datetime)],
         period=10
     )
+
+def add_task():
     db.Task.update_or_insert(_key=dict(name=task.name), **task)
     scheduler.queue_task(run_task, pvars=dict(name=task.name), repeats=0, period=task.period, immediate=True, retry_failed=-1)
     return db().select(db.Task.ALL)
@@ -14,9 +18,13 @@ def delete_all_tasks():
     db.scheduler_task.drop()
     db.scheduler_run.drop()
     db.Task.drop()
+    return True
 
 def get_tasks():
     return scheduler.tasks
 
 def run_task_name():
     return run_task(request.vars.name)
+
+def test():
+    pprint(http_request(task.url, selectors=task.selectors))
--- applications/welcome/models/Controller.py
+++ applications/welcome/models/Controller.py
@@ -6,17 +6,25 @@ from requests import Session  # for login required http requests
 from gluon.storage import Storage  # easy to use dictioanry
 from gluon.scheduler import Scheduler  # for job scheduling
 import json  # for storing of dynamically schemed data
+import feedparser  # autodetection of date formats
+import datetime  # date / time support
 
 db = DAL('sqlite://storage.sqlite', pool_size=1, check_reserved=['all'])
 scheduler = Scheduler(db)
 
+class Selector(object):  # contains information for selecting a ressource on a xml/html page
+    def __init__(self, xpath, type, regex=None):
+        self.xpath = xpath
+        self.regex = regex
+        self.type = type
+
 db.define_table('Task',
-                Field("name", type="string", unique=True),
-                Field("url", type="string"),
-                Field("xpath", type="string"),
-                Field("regex", type="string"),
-                Field("creation_datetime",   type="datetime", default=request.now),
-                Field("period", type="integer", default=10),  # in seconds
+    Field("name", type="string", unique=True),
+    Field("url", type="string"),
+    Field("xpaths", type="list:string"),
+    Field("regexes", type="list:string"),
+    Field("creation_datetime",   type="datetime", default=request.now),
+    Field("period", type="integer", default=10),  # in seconds
 )
 for task in db().select(db.Task.ALL):
     db.define_table(task.name, Field("task_result", type="json"))
@@ -30,18 +38,42 @@ def run_task(name):
     db.commit()
     return result
 
-def parse(html_src, xpath=None, regex=None):
+def parse(html_src, selectors=None):
     """ Parses an html document for a given XPath expression. Any resulting node can optionally be filtered against a regular expression """
-    if not xpath:
+
+    if not selectors:
         return html_src  # nothing to do
 
     parsed_tree = html.document_fromstring(html_src)
-    nodes = parsed_tree.xpath(xpath)
 
-    if regex:
-        return [re.search(regex, str(node),  re.DOTALL).group() for node in [str(node) for node in nodes]]  # apply regex to every single node
+    selector_results = []
+    for selector in selectors:
+        nodes = parsed_tree.xpath(selector.xpath)
 
-    return nodes
+        if selector.type in [unicode, str]:
+            output_cast = selector.type
+            selector.regex = selector.regex or "\w+"
+        elif selector.type == int:
+            output_cast = selector.type
+            selector.regex = selector.regex or "\d+"
+        elif selector.type == float:
+            output_cast = selector.type
+            selector.regex = selector.regex or "\d+\.\d+"
+        elif selector.type is datetime.datetime:
+            output_cast = selector.type = lambda data : datetime.datetime(*(feedparser._parse_date(data)[:6]))
+            selector.regex = selector.regex or "\d+ \w+ \d+"
+
+        if selector.regex:
+            result = [re.search(selector.regex, node,  re.DOTALL | re.UNICODE).group() for node in [unicode(node) for node in nodes] if re.search(selector.regex, node,  re.DOTALL)]  # apply regex to every single node
+
+        ## auto cast result type##
+        result = [output_cast(data) for data in result]
+        selector_results += [result]
+
+    ## convert selector results from a tuple of lists to a list of tuples ##
+    selector_results = [tuple(selector_results[j][i] for j in range(len(selectors))) for i in range(len(selector_results[0]))]
+
+    return selector_results
 
 def login(url, user, password):
     """ Returns the session that is yielded by the login """
@@ -53,10 +85,10 @@ def login(url, user, password):
     session.post(url, data)
     return session
 
-def http_request(url, session=None, xpath=None, regex=None):
+def http_request(url, selectors=None, session=None):
     """ Returns the response of an http get-request to a given url """
     session = session or Session()
     html_src = session.get(url).text
-    return parse(html_src, xpath=xpath, regex=regex)
+    return parse(html_src, selectors=selectors)
 
 
